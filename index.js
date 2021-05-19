f4967bb6f1d266a202f8b2824116f01e9f675919
Vaalrhona <lucille.lvt@gmail.com>
2021-05-19 12:00:41 +0200

    added pgsql

--- index.js
+++ index.js
@@ -4,12 +4,32 @@ var app = express();
 
 require('dotenv').config()
 
+const { Pool } = require('pg');
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
 app.set('port', (process.env.PORT || 5000));
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/public');
 app.get('/cool', (req, res) => res.send(cool()))
-
+app.get('/times', (req, res) => res.send(showTimes()))
+app.get('/db', async(req, res) => {
+  try {
+    const client = await pool.connect();
+    const result = await client.query('SELECT * FROM test_table');
+    const results = { 'results': (result) ? result.rows : null};
+    res.render('pages/db', results);
+    client.release();
+  } catch (err) {
+    console.error(err);
+    res.send("Error " + err);
+  }
+})
 app.get('/', function(request, response) {
   var env = process.env.APP_ENV;
   if (env == 'staging') {
@@ -27,3 +47,12 @@ app.listen(app.get('port'), function() {
 });
 
 module.exports = app
+
+showTimes = () => {
+  let result = '';
+  const times = process.env.TIMES || 5;
+  for(i = 0; i < times; i++){
+    result += i + ' ';
+  }
+  return result;
+}
